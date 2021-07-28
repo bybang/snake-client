0312e81422776be25cf14091c77fb6233042b640
bybang <bybang26@gmail.com>
2021-07-28 14:36:40 -0400

    practice module.exports method to how to import different object keys(=objects) in different file

---
+++ client.js
@@ -0,0 +1,20 @@
+const net = require("net");
+
+const connect = function () {
+  const conn = net.createConnection({
+    host: '135.23.223.133',// IP address here,
+    port: 50542// PORT number here,
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+  
+  conn.on("connect", () => {
+    console.log('successfully connected!')
+    // code that does something when the connection is first established
+  })
+
+  return conn;
+};
+
+module.exports = {connect};
---
+++ play.js
@@ -0,0 +1,7 @@
+// importing file
+const {connect} = require("./client.js"); // {connect: function()... }
+// const connect = imports.connect // means  imports.connect(= const {connect} = require("./client.js");)
+
+
+console.log("Connecting ...");
+connect();
